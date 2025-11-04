dae05479098d8f17f5714354deedf6ee975ef80c
josephshaju2002 <josephshaju466@gmail.com>
2025-11-04 22:39:57 +0530

    contactus updated

--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Contact.css";
 
+
 const Contact = () => {
   return (
-    <section className="contact-section">
+    <section className="contact-section mt-5">
       <div className="contact-container">
         <h2 className="title">Contact Us</h2>
         <p className="subtitle">
